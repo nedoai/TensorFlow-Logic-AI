da41ab6f4194832540495a541abfd8b8c8604271
nedoai <141833766+nedoai@users.noreply.github.com>
2023-08-15 22:35:28 +0300

    Update test.py

--- test.py
+++ test.py
@@ -20,19 +20,15 @@ outputs = [data[1] for data in training_data]
 tokenizer = Tokenizer()
 tokenizer.fit_on_texts(inputs + outputs)
 
-# Преобразование текстовых данных в последовательности чисел
 input_sequences = tokenizer.texts_to_sequences(inputs)
 output_sequences = tokenizer.texts_to_sequences(outputs)
 
-# Подгонка последовательностей до одинаковой длины
 max_seq_length = max(max(map(len, input_sequences)), max(map(len, output_sequences)))
 
-# Пример взаимодействия с моделью
 input_text = "На полу разбитая игрушка"
 input_sequence = tokenizer.texts_to_sequences([input_text])
 input_sequence = pad_sequences(input_sequence, maxlen=max_seq_length, padding='post')
 output_sequence = model.predict(input_sequence)
 
-# Декодирование вывода модели в текст
 decoded_output = tokenizer.sequences_to_texts([np.argmax(output_sequence[0], axis=-1)])[0]
 print("Вывод:", decoded_output)
